5296ab859b5820d24e675820d1e2a054e880dc0e
TheJimmyBlaze <TheJimmyBlaze@gmail.com>
2022-07-23 17:39:52 +1200

    Updating scripts to work with zsh pwsh

--- Scripts/MigrateDB.ps1
+++ Scripts/MigrateDB.ps1
@@ -4,7 +4,10 @@ if ($args.count -eq 0){
     return;
 }
 
-$DB = $PSScriptRoot + "\..\Database\";
-$API = $PSScriptRoot + "\..\API\";
+$DB = $PSScriptRoot + "/../Database/";
+$API = $PSScriptRoot + "/../API/";
+
+write-host "using Database Project: " + $DB;
+write-host "using API Project: " + $API;
 
 dotnet ef migrations add $args[0] --project $DB --startup-project $API
--- Scripts/UpdateDB.ps1
+++ Scripts/UpdateDB.ps1
@@ -1,5 +1,8 @@
 
-$DB = $PSScriptRoot + "\..\Database\";
-$API = $PSScriptRoot + "\..\API\";
+$DB = $PSScriptRoot + "/../Database/";
+$API = $PSScriptRoot + "/../API/";
+
+write-host "using Database Project: " + $DB;
+write-host "using API Project: " + $API;
 
 dotnet ef database update --project $DB --startup-project $API
