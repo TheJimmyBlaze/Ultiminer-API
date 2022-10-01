509c3249ed953a2f205ca126a7a5b39ae9d0e4d2
TheJimmyBlaze <TheJimmyBlaze@gmail.com>
2022-10-02 01:49:20 +1300

    Adding exp to the loot indexes

--- API/Services/Loot/LootTableIndex.cs
+++ API/Services/Loot/LootTableIndex.cs
@@ -13,9 +13,14 @@ namespace Services.Loot {
         private readonly Random random;
         private readonly IDbContextFactory<UltiminerContext> databaseFactory;
 
+        private struct ResourceIndex {
+            public string NaturalId {get; set;}
+            public int Exp {get; set;}
+        }
+
         private struct NodeIndex {
             public int Quantity {get; set;}
-            public IDictionary<double, string> Index {get; set;}
+            public IDictionary<double, ResourceIndex> Index {get; set;}
         }
         private readonly Dictionary<string, NodeIndex> index = new();
 
@@ -37,24 +42,25 @@ namespace Services.Loot {
             if (index.TryGetValue(nodeId, out NodeIndex nodeIndex)) {
                 
                 //Store it as a dictionary during generation, this makes it easier to increment a resource by id
-                Dictionary<string, int> rawLoot = new();
+                Dictionary<ResourceIndex, int> rawLoot = new();
 
                 int quantity = random.Next(nodeIndex.Quantity);
                 for(int roll = 0; roll <= quantity; roll++) {
 
                     double lootKey = random.NextDouble();
-                    string lootId = nodeIndex.Index.First(index => index.Key >= lootKey).Value;
+                    ResourceIndex resource = nodeIndex.Index.First(index => index.Key >= lootKey).Value;
 
-                    if (!rawLoot.ContainsKey(lootId)) {
-                        rawLoot[lootId] = 1;
+                    if (!rawLoot.ContainsKey(resource)) {
+                        rawLoot[resource] = 1;
                     } else {
-                        rawLoot[lootId] ++;
+                        rawLoot[resource] ++;
                     }
                 }
 
                 //Convert the dictionary to a list of Resource Stacks
                 List<ResourceStack> resources = rawLoot.Select(raw => new ResourceStack(){
-                    ResourceId = raw.Key,
+                    ResourceId = raw.Key.NaturalId,
+                    ExperienceAwarded = raw.Key.Exp,
                     Count = raw.Value
                 }).ToList();
 
@@ -77,6 +83,9 @@ namespace Services.Loot {
 
             //Get a db context
             using UltiminerContext database = databaseFactory.CreateDbContext();
+
+            //Build the exp index, we'll use this when we finish up each index
+            Dictionary<string, int> expIndex = buildExperienceIndex(database);
 
             //Build the index for every node
             IEnumerable<Node> nodes = database.Nodes
@@ -106,7 +115,7 @@ namespace Services.Loot {
 
                 //Build a weight index for the table
                 IEnumerable<int> rawRarities = sorted.Select(rarity => rarity.Value);
-                Dictionary<int, int> weightIndex = BuildWeightIndex(rawRarities);
+                Dictionary<int, int> weightIndex = buildWeightIndex(rawRarities);
 
                 //Replace each rarity with it's indexed weight
                 IEnumerable<int> weights = sorted.Select(resource => weightIndex[resource.Value]);
@@ -119,9 +128,14 @@ namespace Services.Loot {
                 IEnumerable<double> triangular = percentages.Select((_, i) => percentages.Take(i + 1).Sum());
 
                 //Create the percentage index by pairing the triangular percentage with it's resource ID
-                IDictionary<double, string> indexData = triangular
+                IDictionary<double, ResourceIndex> indexData = triangular
                     .Zip(sorted, (triangular, resource) => new KeyValuePair<double, string>(triangular, resource.Key))
-                    .ToDictionary(index => index.Key, index => index.Value);
+                    .ToDictionary(index => index.Key, index => {
+                        return new ResourceIndex(){
+                            NaturalId = index.Value,
+                            Exp = expIndex[index.Value]
+                        };
+                    });
 
                 //Add the node index to the loot index
                 NodeIndex nodeIndex = new(){
@@ -135,7 +149,7 @@ namespace Services.Loot {
             logger.LogInformation("Finished building all Loot Table Indexes after: {TotalTimerMS}", totalTimer.Elapsed);
         }
 
-        private static Dictionary<int, int> BuildWeightIndex(IEnumerable<int> rarities) {
+        private static Dictionary<int, int> buildWeightIndex(IEnumerable<int> rarities) {
 
             //Get each unique rarity on the table
             IEnumerable<int> distinct = rarities.Distinct();
@@ -152,6 +166,16 @@ namespace Services.Loot {
                 .ToDictionary(index => index.Key, index => index.Value);
 
             return weightIndex;
+        }
+
+        private static Dictionary<string, int> buildExperienceIndex(UltiminerContext database) {
+
+            //Get a dictionary where the key is the resource Id, and the value is the experience awarded
+            Dictionary<string, int> expIndex = database.Resources
+                .Select(resource => new KeyValuePair<string, int>(resource.NaturalId, resource.ExperienceAwarded))
+                .ToDictionary(resource => resource.Key, resource => resource.Value);
+
+            return expIndex;
         }
     }
 }
